03f86870c6b2bef35e0925b41f20e601ac13580f
Gasper Habjan <gapy5000@gmail.com>
2024-01-05 02:17:03 +0100

    Fix config.yaml file.

--- api/src/main/java/si/fri/rso/zapeljise/api/v1/RideApplication.java
+++ api/src/main/java/si/fri/rso/zapeljise/api/v1/RideApplication.java
@@ -7,7 +7,6 @@ import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import si.fri.rso.zapeljise.api.v1.resources.DemoResource;
 import si.fri.rso.zapeljise.api.v1.resources.RideDataResource;
-
 import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
@@ -16,7 +15,7 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "API for rides", version = "v1",
         contact = @Contact(email = "gh6987@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing rides."),
-        servers = @Server(url = "http://localhost:8080/"))
+        servers = @Server(url = "http://4.236.250.26/ms-ride"))
 @ApplicationPath("/v1")
 public class RideApplication extends Application {
     @Override
