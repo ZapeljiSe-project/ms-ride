d155ec94b442cb5e662c0c7bf1b40a1231ee45e0
Gasper Habjan <gapy5000@gmail.com>
2024-01-05 01:55:55 +0100

    Fix config.yaml file.

--- api/src/main/java/si/fri/rso/zapeljise/api/v1/RideApplication.java
+++ api/src/main/java/si/fri/rso/zapeljise/api/v1/RideApplication.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.servers.Server;
+import si.fri.rso.zapeljise.api.v1.resources.DemoResource;
 import si.fri.rso.zapeljise.api.v1.resources.RideDataResource;
 
 import java.util.HashSet;
@@ -15,7 +16,7 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "API for rides", version = "v1",
         contact = @Contact(email = "gh6987@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing rides."),
-        servers = @Server(url = "http://4.236.250.26/ms-ride"))
+        servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("/v1")
 public class RideApplication extends Application {
     @Override
@@ -23,6 +24,7 @@ public class RideApplication extends Application {
         Set<Class<?>> classes = new HashSet<>();
         // Add your JAX-RS resource classes
         classes.add(RideDataResource.class);
+        classes.add(DemoResource.class);
         // Add the CorsFilter class
         classes.add(CorsFilter.class);
         return classes;
